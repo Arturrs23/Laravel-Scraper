52f5bea8445feefa234a4a601d773a6d8306c78c
Arturs <arrturs.torsters@gmail.com>
2023-01-27 15:43:53 +0200

    Table data

--- resources/js/Pages/News/Index.vue
+++ resources/js/Pages/News/Index.vue
@@ -8,12 +8,28 @@ import axios from "axios";
 </script>
 <template>
     <AuthenticatedLayout>
-        <div>
+        <div class="p-28">
             <table class="min-w-full leading-normal">
+                <thead class="text-left">
+                    <tr>
+                      <th class="text-white px-5 py-5 text-sm">
+                      <h3>Name</h3>
+                      </th>
+                      <th class="text-white px-5 py-5 text-sm">
+                       <h3>Link</h3>
+                      </th>
+                      <th class="text-white px-5 py-5 text-sm">
+                       <h3>Points</h3>
+                      </th>
+                      <th class="text-white px-5 py-5 text-sm">
+                    <h3>Date</h3>
+                      </th>
+                    </tr>
+                  </thead>
                 <!-- using var data_list to loop trough and display-->
                 <tr class="border border-white-500" v-for="data in data_list" :key="data.id">
                     <td class="text-white px-5 py-5 text-sm">{{ data.title }}</td>
-                    <td class="text-white px-5 py-5 text-sm"><a :href="data.link" target="_blank">{{ data.link }}</a></td>
+                    <td class="text-white px-5 py-5 text-sm hover:underline"><a :href="data.link" target="_blank">{{ data.link }}</a></td>
                     <td class="text-white px-5 py-5 text-sm">{{ data.points }}</td>
                     <td class="text-white px-5 py-5 text-sm">{{ data.date_created }}</td>
                 </tr>
