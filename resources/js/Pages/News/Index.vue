8fffd243d58bc21c73a4af7c868992704b58ef1d
Arturs <arrturs.torsters@gmail.com>
2023-01-27 15:31:10 +0200

    links

--- resources/js/Pages/News/Index.vue
+++ resources/js/Pages/News/Index.vue
@@ -1,32 +1,28 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import { useForm, Head } from '@inertiajs/vue3';
-import Return from '@/Components/Return.vue';
-import axios from 'axios'
-
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import InputError from "@/Components/InputError.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import { useForm, Head } from "@inertiajs/vue3";
+import Return from "@/Components/Return.vue";
+import axios from "axios";
 </script>
 <template>
     <AuthenticatedLayout>
         <div>
-            <table>
+            <table class="min-w-full leading-normal">
                 <!-- using var data_list to loop trough and display-->
-                <tr v-for=" data in data_list" :key="data.id">
-                  <td class="text-white">{{ data.title }}</td>
-                  <td class="text-white">{{ data.link }}</td>
-                  <td class="text-white">{{ data.points }}</td>
-                  <td class="text-white">{{ data.date_created }}</td>
+                <tr class="border border-white-500" v-for="data in data_list" :key="data.id">
+                    <td class="text-white px-5 py-5 text-sm">{{ data.title }}</td>
+                    <td class="text-white px-5 py-5 text-sm"><a :href="data.link" target="_blank">{{ data.link }}</a></td>
+                    <td class="text-white px-5 py-5 text-sm">{{ data.points }}</td>
+                    <td class="text-white px-5 py-5 text-sm">{{ data.date_created }}</td>
                 </tr>
-              </table>
+            </table>
         </div>
     </AuthenticatedLayout>
-
-  </template>
-  
-  <script>
-  
-  export default {
-  props:['data_list']
-}
-  </script>
+</template>
+<script>
+export default {
+    props: ["data_list"],
+};
+</script>
